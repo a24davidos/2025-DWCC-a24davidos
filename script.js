b6c6ea6970a5c14e7433b3c7acf809b2b7ab8939
a24davidos <a24davidos@iessanclemente.net>
2025-09-23 11:13:55 +0200

    Primeiros exercicios

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Meu neno");
+"use strict";
+
+// Exercicios;
+
+// 1.Crea unha variable que almacene un día da semana de luns a domingo. En función do valor da variable mostra unha mensaxe indicando se o día é laborable ou non
+
+const nonLaborable = ["sábado", "domingo"];
+
+console.log("EXERCICIO 1");
+
+let diaSemana = "luns";
+
+if (!(diaSemana in nonLaborable)) console.log("É laborable");
+else {
+  console.log("Non é laborable");
+}
+
+// 2.Crea 3 variables e inicialízaas con 3 números diferentes. Mostra por pantalla o maior dos 3 números
+
+console.log("EXERCICIO 2");
