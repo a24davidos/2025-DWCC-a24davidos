a2dd202e219c9738e8a4e547b465fdcfdca0490b
a24davidos <a24davidos@iessanclemente.net>
2025-10-14 11:41:13 +0200

    commmit

--- funcions2.js
+++ funcions2.js
@@ -10,22 +10,8 @@ console.log(suma);
 
 //2. Utilizando a función reduce, obtén o valor mínimo dun array de números.
 
-const array = [1, 2, 3, 4, 5];
-function reducer(accumulator, currentValue, index) {
-  const returns = accumulator + currentValue;
-  console.log(
-    `accumulator: ${accumulator}, currentValue: ${currentValue}, index: ${index}, returns: ${returns}`
-  );
-  return returns;
-}
-let resultado = array.reduce(reducer);
-console.log(`Resultado = ${resultado}`);
-console.log("Usando un valor inicial");
-resultado = array.reduce(reducer, 0);
-console.log(`Resultado = ${resultado}`);
-console.log("Usando unha función frecha");
-resultado = array.reduce(
-  (accumulator, currentValue, index) => accumulator + currentValue,
-  0
-);
-console.log(`Resultado = ${resultado}`);
+const array = [2, 3, 1, 4, 6];
+
+const reducer = (a, c) => (a > c ? (a = c) : a);
+
+console.log(a.reduce(reducer));
