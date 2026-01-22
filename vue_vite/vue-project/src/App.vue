1c444e2ccd2e09f61d537e79a0236d3ef7b54342
a24davidos <a24davidos@iessanclemente.net>
2026-01-22 11:08:17 +0100

    as

---
+++ vue_vite/vue-project/src/App.vue
@@ -0,0 +1,30 @@
+<script>
+import FoodItem from "./components/FoodItem.vue";
+
+export default {
+  components: {
+    FoodItem,
+  },
+  data() {
+    return {
+      message: "Ola mundo",
+      count: 0,
+      name: "Apples",
+      foodMessage: "I like apples",
+    };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <button @click="count++">You clicked me {{ count }} times.</button>
+
+    <FoodItem :name="name" :message="foodMessage" />
+  </div>
+</template>
+
+<style scoped>
+/* tu CSS */
+</style>
---
+++ vue_vite/vue-project/src/components/FoodItem.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  props: {
+    name: String,
+    message: String,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h2>{{ name }}</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<style scoped>
+/* CSS opcional */
+</style>
