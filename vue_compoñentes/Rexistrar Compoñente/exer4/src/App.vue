52093ae71b9bb3d59d82caeb8e4a5cd0a0a440dd
DavidOteros <a24davidos@iessanclemente.net>
2026-01-28 22:08:02 +0100

    Ejercicios de Compoñentes VUE

---
+++ vue_compoñentes/Rexistrar Compoñente/exer4/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <Contact v-for="c in contactos" :nome="c.nome" :telefono="c.telefono" :correo="c.correo" :favorito="c.favorito"/>
+</template>
+
+<script>
+import Contact from './components/Contact.vue';
+
+export default {
+  components: {Contact},
+  data () {
+    return{
+      contactos: [{
+        nome: "David", telefono: "698143320", correo: "a@a.com", favorito: true
+      },{
+        nome: "Marga", telefono: "696383310", correo: "b@b.com", favorito: false
+
+      }]
+    }
+
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ vue_compoñentes/Rexistrar Compoñente/exer4/src/components/Contact.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="contact">
+    <h2>{{ nome }} <span v-if="favorito">(Favorito) ★ </span></h2>
+    <button @click="toggleDetalles">{{ textoBotonInterno }}</button>
+    <div v-if="mostrarInterno">
+      <p>Teléfono: {{ telefono }}</p>
+      <p>Correo: {{ correo }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    nome: String,
+    telefono: String, 
+    correo: String,
+    favorito: Boolean,
+  },
+  data() {
+    return {
+      mostrarInterno: false,
+    }
+  },
+  computed: {
+    textoBotonInterno() {
+      return this.mostrarInterno ? "Ocultar detalles" : "Mostrar detalles";
+    }
+  },
+  methods: {
+    toggleDetalles() {
+      this.mostrarInterno = !this.mostrarInterno;
+    }
+  }
+}
+</script>
