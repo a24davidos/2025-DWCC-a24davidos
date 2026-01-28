f2b990dd3b0440f3f3204807beb15de098835a2d
DavidOteros <a24davidos@iessanclemente.net>
2026-01-28 22:08:15 +0100

    subir

---
+++ vue_compoñentes/Rexistrar Compoñente/exer3/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import Contact from "./components/Contact.vue";
+
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+<template>
+  <div>
+    <h1>Listado de contactos</h1>
+    <Contact/>
+  </div>
+</template>
+<style></style>
---
+++ vue_compoñentes/Rexistrar Compoñente/exer3/src/components/Contact.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="contact">
+    <h2>{{ contacto.nome }}</h2>
+    <button @click="toggleDetalles">{{ textoBoton }}</button>
+    <div v-if="mostrar">
+      <p>Teléfono: {{ contacto.telefono }}</p>
+      <p>Correo: {{ contacto.correo }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Contact",
+  data() {
+    return {
+      contacto: {
+        nome: "David",
+        telefono: "698143320",
+        correo: "a@a.com"
+      },
+      mostrar: false,
+      textoBoton: "Mostrar detalles"
+    }
+  },
+  methods: {
+    toggleDetalles() {
+      this.mostrar = !this.mostrar
+      this.textoBoton = this.mostrar ? "Ocultar detalles" : "Mostrar detalles"
+    }
+  }
+}
+</script>
