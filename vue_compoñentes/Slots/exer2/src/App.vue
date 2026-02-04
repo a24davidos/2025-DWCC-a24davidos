138dd80ae4af155c788072ea3d3626c8373c4426
DavidOteros <a24davidos@iessanclemente.net>
2026-02-04 22:00:40 +0100

    Exercicios Slots feitos

---
+++ vue_compoñentes/Slots/exer2/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+<button @click="show">Mostrar</button>
+<!-- <Modal :visible="visible" @close="closeModal"/> -->
+
+
+<!-- Esta versión para ver como sobrescribir -->
+<Modal :visible="visible" @close="closeModal">
+      <!-- Slot header -->
+      <template #header>
+        <h3>Encabezado personalizado</h3>
+      </template>
+
+      <!-- Slot body -->
+      <template #body>
+        <p>Este es el contenido personalizado del cuerpo de la modal.</p>
+      </template>
+
+      <!-- Slot footer -->
+      <template #footer>
+       <p>Contenido personalizado para el footer</p>
+      </template>
+    </Modal>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      visible: false
+    };
+  }, methods:{
+    show(){
+      this.visible = true
+    },
+    closeModal(){
+      this.visible = false
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ vue_compoñentes/Slots/exer2/src/components/Modal.vue
@@ -0,0 +1,53 @@
+<template>
+  <div v-if="visible" class="modal-mask">
+    <div class="modal-container">
+      <slot name="header">
+        <h3>Default Header</h3>
+      </slot>
+      <slot name="body">
+        <p>Default body</p>
+      </slot>
+      <slot name="footer" >
+        <p>Default footer</p>
+      </slot>
+      <button @click="$emit('close')">Ok</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+}
+
+.modal-container {
+  width: 300px;
+  margin: auto;
+  padding: 20px 30px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+}
+</style>
+
+<script>
+export default {
+  props:{
+    visible:{
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+    };
+  }
+};
+</script>
